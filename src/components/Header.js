e853cee60b8a12e15b54d0460570c58966f976bf
Ebenezer <Ebenezer>
2021-11-08 17:19:42 -0800

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -79,7 +79,7 @@ function Header() {
 
             {/* Right section of header*/}
             <div className="flex items-center space-x-4 justify-end text-gray-600">
-                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                {/*<p className="hidden md:inline cursor-pointer">Become a host</p>*/}
                 <GlobeAltIcon className="h-6 cursor-pointer"/>
 
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
